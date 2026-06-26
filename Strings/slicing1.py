ad788602e275145237b8557e03e1f0f11ecc0a8e
coder-vaishnavi <deshmukhvaishnavi1907@gmail.com>
2026-06-26 22:50:28 +0530

    Day-5 completed

---
+++ Strings/slicing1.py
@@ -0,0 +1,8 @@
+# Given text = "Python Programming", do the following:
+# Print the first 6 characters
+# Print the last 6 characters
+# Print every second character from the string
+text = "Python Programming"
+print(f"the first 6 characters {text[:6]}")
+print(f"the last 6 characters {text[-6:]}")
+print(f"every second character from the string {text[::2]}")
---
+++ Strings/slicing2.py
@@ -0,0 +1,3 @@
+# Reverse the string text using slicing.
+string = "vaishnavi"
+print(f"reverse of {string} is {string[::-1]} ") #step = -1 → move backwards one character at a time

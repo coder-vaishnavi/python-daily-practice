3719340f386fdde3bd889f9cdc9814f23b5e1164
coder-vaishnavi <deshmukhvaishnavi1907@gmail.com>
2026-06-27 17:19:27 +0530

    question3 solved

---
+++ Strings/f-string.py
@@ -0,0 +1,11 @@
+# Using format(), create a sentence:
+# "My name is John and I am 25 years old."
+# by passing "John" and 25 as variables.
+name = "John"
+age = 25
+result = "My name is {} and I am {} years old.".format(name, age)
+print(result)
+
+# using f-string
+print("using f-string")
+print(f"my name is {name} and I am {age} years old")

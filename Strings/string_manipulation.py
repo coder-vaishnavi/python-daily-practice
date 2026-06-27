9955e29cdae3c1aaf34e9c0938f3ca76f0fb2bad
coder-vaishnavi <deshmukhvaishnavi1907@gmail.com>
2026-06-27 17:24:51 +0530

    question-4 solved

---
+++ Strings/string_manipulation.py
@@ -0,0 +1,5 @@
+# Given sentence = "Coding in Python is fun", replace "fun" with "awesome" and print it.
+# Find the index of the word "Python" in sentence.
+# Convert the entire sentence to uppercase and print it.
+sentence = "Coding in Python is fun"
+print(f"sting is: {sentence} \nreplace 'fun' with 'awesome' \nafter replacing: {sentence.replace('fun', 'awesome')}")

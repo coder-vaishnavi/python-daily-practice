a69f51e7294aab347d418166c92bfdb71e54ed90
coder-vaishnavi <deshmukhvaishnavi1907@gmail.com>
2026-06-29 20:17:22 +0530

    Q1 solved

---
+++ DSA/list1.py
@@ -0,0 +1,19 @@
+# Create a list fruits = ["apple", "banana", "cherry"].
+
+# Print the first fruit.
+# Replace "banana" with "orange".
+# Print the length of the list.
+fruits = ["apple", "banana", "cherry"]
+print(f"first fruit {fruits[0]}")
+fruits[1] = "orange"
+print(f"after Replace 'banana' with 'orange' final list is {fruits}")
+print(f"the length of the list {len(fruits)}")
+# Create a list of numbers from 1 to 10.
+
+# Print the first three numbers using slicing.
+# Print the last three numbers using slicing.
+
+list = [1,2,3,4,5,6,7,8,9,10]
+print(f"the first three numbers using slicing {list[:3]}")
+print(f"the last three numbers using slicing {list[-3:]}")
+

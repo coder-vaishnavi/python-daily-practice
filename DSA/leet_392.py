ab9dba208c7f6324e2f74e04b8ad0daaccafb295
coder-vaishnavi <deshmukhvaishnavi1907@gmail.com>
2026-07-31 21:46:32 +0530

    question solved

---
+++ DSA/leet_392.py
@@ -0,0 +1,17 @@
+s = input("Enter s: ")
+t = input("Enter t: ")
+
+i = 0
+j = 0
+
+while i < len(s) and j < len(t):
+    if s[i] == t[j]:
+        i += 1
+        j += 1
+    else:
+        j += 1
+
+if i == len(s):
+    print(True)
+else:
+    print(False)
